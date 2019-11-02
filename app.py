758b9c019642cc60c33fd8a03b565dbda82fc3e6
theriley106 <christopherlambert106@gmail.com>
2019-11-01 22:15:29 -0700

    Added flask app part to get post request data

--- app.py
+++ app.py
@@ -3,6 +3,15 @@ from spotify import parseURL
 
 app = Flask(__name__, static_url_path='/static')
 
+@app.route('/', methods=['GET', "POST"])
+def index():
+	data = request.get_json()
+	url = data['url']
+	returnedData = parseURL(url)
+	return jsonify(returnedData.return_values())
+	print request.get_json()
+	return jsonify(data)
+
 @app.route('/test', methods=['GET'])
 def testPage():
 	return render_template("index1.html")
