d5e1207b1d84f9f05d23d9cf02a626f2732362b0
Alex Haight <ahaight@clemson.edu>
2019-11-02 09:47:42 -0400

    morning commit

--- queuedUp/queuedUpSE/ShareViewController.swift
+++ queuedUp/queuedUpSE/ShareViewController.swift
@@ -20,7 +20,11 @@ class ShareViewController: SLComposeServiceViewController {
         // This is called after the user selects Post. Do the upload of contentText and/or NSExtensionContext attachments.
     
         // Inform the host that we're done, so it un-blocks its UI. Note: Alternatively you could call super's -didSelectPost, which will similarly complete the extension context.
+        //let url = URL(string: "https://ptsv2.com/t/ngiaa-1572674328/post")!
+        //var request = URLRequest(url: url)
         self.extensionContext!.completeRequest(returningItems: [], completionHandler: nil)
+        print("hey there");
+        print(self.extensionContext!.inputItems[0]);
     }
 
     override func configurationItems() -> [Any]! {
